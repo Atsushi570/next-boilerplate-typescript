96de6b56b8c1572f613bf3ef44a310e53461fd52
Atsushi Sato <sato.atsushi570@gmail.com>
2022-02-16 18:29:06 +0900

    ボタンコンポーネントを追加

---
+++ src/components/basics/Button.tsx
@@ -0,0 +1,21 @@
+import { FC, ReactNode } from 'react'
+import MuiButton, { ButtonProps } from '@mui/material/Button'
+
+type Props = ButtonProps & {
+  children?: ReactNode
+  styles?: { [key: string]: string }
+}
+
+export const Button: FC<Props> = (props: Props) => {
+  const {
+    children,
+    variant = 'outlined',
+    styles = { 'text-transform': 'none' },
+    ...muiButtonProps
+  } = props
+  return (
+    <MuiButton variant={variant} style={styles} {...muiButtonProps}>
+      {children}
+    </MuiButton>
+  )
+}
