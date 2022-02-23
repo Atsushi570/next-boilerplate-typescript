ab7a7472d21c0a9a67f6bcec4905f14c5ffd1ef9
Atsushi Sato <sato.atsushi570@gmail.com>
2022-02-23 17:49:27 +0900

    storybookでscssを読み込めるよう修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -14,6 +16,25 @@ module.exports = {
     },
   ],
   framework: '@storybook/react',
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              auto: true,
+            },
+          },
+        },
+        'sass-loader',
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    return config
+  },
   core: {
     builder: 'webpack5',
   },
