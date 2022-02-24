ed0f313e739362f12de863e6ff8401ef9cd0cbef
Atsushi Sato <sato.atsushi570@gmail.com>
2022-02-24 21:30:46 +0900

    storybook起動時に@がsrcのエイリアスになるよう設定を追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -17,6 +17,10 @@ module.exports = {
   ],
   framework: '@storybook/react',
   webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    }
     config.module.rules.push({
       test: /\.scss$/,
       use: [
