579f1692f0fb38064e41b2061dda92e3a2c8c1a6
Atsushi Sato <sato.atsushi570@gmail.com>
2022-02-24 21:31:24 +0900

    Muiコンポーネントのネストされた要素のスタイル変更例を追加

--- src/components/basics/Input/Input.tsx
+++ src/components/basics/Input/Input.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from 'react'
+import { makeStyles } from '@mui/styles'
 import MuiInput, { TextFieldProps } from '@mui/material/TextField'
 import Colors from '@/styles/themes/colors'
 
@@ -8,10 +9,20 @@ type Props = Omit<TextFieldProps, 'color'> & {
   color?: keyof typeof Colors
 }
 
+const useStyles = makeStyles(() => ({
+  input: {
+    '& .Mui-focused': {
+      backgroundColor: () => Colors.danger,
+    },
+  },
+}))
+
 export const Input: FC<Props> = (props: Props) => {
   const { children, variant = 'outlined', size = 'small', ...muiTextFieldProps } = props as any
+  const classes = useStyles()
+
   return (
-    <MuiInput variant={variant} size={size} {...muiTextFieldProps}>
+    <MuiInput className={classes.input} variant={variant} size={size} {...muiTextFieldProps}>
       {children}
     </MuiInput>
   )
